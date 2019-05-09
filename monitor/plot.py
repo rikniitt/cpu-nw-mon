81fd170f7f74f2834ac5fd0f7edcc7c3aa5e32c8
Riku Mäki <riku.niittymaki@gmail.com>
2019-05-09 14:18:53 +0300

    Plot

--- monitor/plot.py
+++ monitor/plot.py
@@ -10,6 +10,8 @@ def observations(observations):
     y_users = np.array([obs["users"] for obs in observations], dtype=int)
     y_load = np.array([obs["load_average_1"] for obs in observations])
     y_cons = np.array([obs["network_connections"] for obs in observations], dtype=int)
+    y_pscnt = np.array([obs["processes"] for obs in observations], dtype=int)
+    y_free = np.array([obs["free_memory"] for obs in observations], dtype=int)
 
     x_labels = [datetime.strptime(obs["created_at"], "%Y-%m-%d %H:%M:%S") for obs in observations]
 
@@ -18,19 +20,29 @@ def observations(observations):
         "From %s to %s" % (x_labels[0].strftime("%Y-%m-%d %H:%M:%S"), x_labels[-1].strftime("%Y-%m-%d %H:%M:%S"))
     )
 
-    axes = plt.subplot(311)
+    axes = plt.subplot(321)
     axes.yaxis.set_major_locator(MaxNLocator(integer=True))
     plt.plot(x_labels, y_users, "b-")
     plt.ylabel("users")
 
-    plt.subplot(312)
-    plt.plot(x_labels, y_load, "c--")
+    plt.subplot(322)
+    plt.plot(x_labels, y_load, "c-")
     plt.ylabel("load average")
 
-    axes = plt.subplot(313)
+    axes = plt.subplot(323)
     axes.yaxis.set_major_locator(MaxNLocator(integer=True))
-    plt.plot(x_labels, y_cons, "m-.")
+    plt.plot(x_labels, y_cons, "m-")
     plt.ylabel("nw connections")
+
+    axes = plt.subplot(324)
+    axes.yaxis.set_major_locator(MaxNLocator(integer=True))
+    plt.plot(x_labels, y_pscnt, "g-")
+    plt.ylabel("ps count")
+
+    axes = plt.subplot(325)
+    axes.yaxis.set_major_locator(MaxNLocator(integer=True))
+    plt.plot(x_labels, y_free, "y-")
+    plt.ylabel("free memory")
 
     plt.gcf().autofmt_xdate()
 
